e7db8b9ecfb0eb6c20a5f0592d16fd09674c5da8
Hugh <hbilston1@gmail.com>
2025-12-19 16:11:22 +1100

    Add initial HTML, CSS, and JS content

---
+++ script.js
@@ -0,0 +1,4 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const countEl = document.getElementById("count");
+  countEl.innerText = "0"; // placeholder until we connect the Azure Function API
+});
